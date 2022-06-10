9c75ee1bfdbc3eecf7369a2350569b9ca008375a
choko0916 <n.yuka0702@gmail.com>
2022-06-11 02:41:37 +0900

    ユーザーのプロフィール設定で、ログイン中のユーザーのidと編集したいユーザーのidが等しいか判定するようにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
   
+  before_action :check_whether_current_user
+
   def edit
     @user = current_user
   end
 
   def update
-    @user = User.find(params[:id])
+    @user = current_user
     if @user.update(user_params)
       flash[:notice] = "プロフィールを更新しました"
       redirect_to  home_index_path
@@ -19,6 +21,13 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:avatar, :name, :introduction).merge(id: current_user.id)
+  end
+
+  def check_whether_current_user
+    if current_user.id != params[:id].to_i #ログイン中のユーザーのidと編集したいユーザーのidが等しいか判定
+      flash[:notice] = "権限がありません"
+      redirect_to home_index_path
+    end
   end
 
 end
